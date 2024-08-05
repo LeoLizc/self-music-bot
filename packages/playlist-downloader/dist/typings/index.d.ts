7971f36543d0b9f5aa2e2dc84bdb90826e408dfc
LeoLizc <leolizcanop@gmail.com>
2024-08-05 00:57:48 -0500

    refactor(packages/playlist-downloader): Remove dist from git

--- packages/playlist-downloader/dist/typings/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { type VideoOptions } from './core/entities/options';
-import { YoutubeService } from './infrastructure/service/youtube';
-import { PlayList } from './interface/playlist';
-export declare const pldl: {
-    (url: string, options?: VideoOptions): Promise<PlayList>;
-    service: YoutubeService;
-};
-export { PlayList } from './interface/playlist';
-export { Video } from './interface/video';
