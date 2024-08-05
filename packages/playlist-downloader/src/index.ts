d4972a0231f91fb79f52f066ba31926d6c4e0ea9
LeoLizc <leolizcanop@gmail.com>
2024-08-04 23:08:39 -0500

    feat(packages/playlist-downloader): pldl now is able to receive PlaylistOptions
    BREAKING CHANGES:
    
    pldl now receives its own type of option

--- packages/playlist-downloader/src/index.ts
+++ packages/playlist-downloader/src/index.ts
@@ -1,13 +1,21 @@
 /* eslint-disable canonical/filename-match-exported */
-import { type VideoOptions } from './core/entities/options';
+import {
+  type PlaylistOptions,
+  type VideoOptions,
+} from './core/entities/options';
 import { YoutubeService } from './infrastructure/service/youtube';
 import { PlayList } from './interface/playlist';
 
 const service = new YoutubeService();
 
-export const pldl = async (url: string, options?: VideoOptions) => {
-  const playlist = await service.getPlaylist(url);
-  return new PlayList(playlist, service, options);
+interface PldlOptionsType {
+  playlistOptions?: PlaylistOptions;
+  videoOptions?: VideoOptions;
+}
+
+export const pldl = async (url: string, options?: PldlOptionsType) => {
+  const playlist = await service.getPlaylist(url, options?.playlistOptions);
+  return new PlayList(playlist, service, options?.videoOptions);
 };
 
 pldl.service = service;
