71830ce66dc3440c8cf3908194fa12e1e49840b9
Leonardo Lizcano <74639893+LeoLizc@users.noreply.github.com>
2024-08-24 03:52:23 +0000

    feat(packages/discord-bot): add pause and resume commands, shuffle command, and update player module
    feat: add pause and resume commands, shuffle command, and update player module in the discord-bot package
    
    The recent user commits indicate that the developer has added pause and resume commands, shuffle command, and made updates to the player module in the discord-bot package. The recent repository commits also confirm these changes. Based on this information, the suggested commit message is "feat: add pause and resume commands, shuffle command, and update player module in the discord-bot package".

--- packages/discord-bot/src/player/index.ts
+++ packages/discord-bot/src/player/index.ts
@@ -1,12 +1,18 @@
 import { Module } from '../core';
 import { disconnectCommand } from './commands/disconnect';
 import { nextCommand } from './commands/next';
+import { pauseCommand } from './commands/pause';
 import { playCommand } from './commands/play';
 import { playlistCommand } from './commands/playlist';
+import { resumeCommand } from './commands/resume';
+import { shuffleCommand } from './commands/shuffle';
 
 export const playerModule = new Module('reproductor', [
   playCommand,
   disconnectCommand,
   nextCommand,
   playlistCommand,
+  shuffleCommand,
+  pauseCommand,
+  resumeCommand,
 ]);
