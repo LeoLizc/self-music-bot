8045e68f6eb81decc39f0ff561b8f63087657e54
Leonardo Lizcano <74639893+LeoLizc@users.noreply.github.com>
2024-08-24 03:47:25 +0000

    feat(packages/discord-bot): add shuffle command for playlist

---
+++ packages/discord-bot/src/player/commands/shuffle.ts
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder } from '../../core';
+import { playlistAdministrator as pa } from '../services/administrator';
+import { type GuildMember } from 'discord.js';
+
+const builder = new SlashCommandBuilder()
+  .setName('shuffle')
+  .setDescription('Reordena la lista de reproducción');
+
+builder.setAction(async (interaction) => {
+  const channel = (interaction.member as GuildMember)?.voice.channel;
+
+  if (!channel) {
+    await interaction.reply('Debes estar en un canal de voz');
+    return;
+  }
+
+  const playlist = pa.get(channel);
+
+  if (!playlist) {
+    await interaction.reply('No hay canciones en la lista');
+    return;
+  }
+
+  await interaction.deferReply();
+
+  playlist.shuffle();
+  await interaction.editReply('Lista de reproducción reordenada');
+});
+
+export const shuffleCommand = builder;
